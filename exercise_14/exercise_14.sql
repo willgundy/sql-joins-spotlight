a972c24162dd225c7871c5ea28dda7c1862f0a31
Will Gundy <willgunderson503@gmail.com>
2022-06-15 11:09:17 -0700

    figured out issue with 14 (had limit 10) and finished 15

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -4,5 +4,4 @@ select c.name, avg(f.length)
 from film f
 join film_category fc on f.film_id = fc.film_id
 join category c on c.category_id = fc.category_id
-group by c.name
-limit 10;
+group by c.name;
--- exercise_15/exercise_15.sql
+++ exercise_15/exercise_15.sql
@@ -1,1 +1,4 @@
 -- the total run time of all inventory items if they were all played back to back
+select sum(f.length)
+from inventory i
+join film f on i.film_id = f.film_id;
