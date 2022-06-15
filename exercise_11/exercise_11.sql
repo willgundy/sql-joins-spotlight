065c8a8f021772dd90c37a234dd10125470084a2
Will Gundy <willgunderson503@gmail.com>
2022-06-15 10:43:02 -0700

    finished #11

--- exercise_11/exercise_11.sql
+++ exercise_11/exercise_11.sql
@@ -3,3 +3,10 @@
 -- GROUP BY the film_id
 -- ORDER in descending order
 -- LIMIT 10
+select f.title, count(*)
+from film f
+left join inventory i on i.film_id = f.film_id
+left join rental r on r.inventory_id = i.inventory_id
+group by f.film_id
+order by 2 desc
+limit 10;
