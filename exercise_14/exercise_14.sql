7b7a82995ad4807412d8692adbb94092b58fd9d5
Will Gundy <willgunderson503@gmail.com>
2022-06-15 11:02:54 -0700

    I think I have 14 working, but can't get test to pass

--- exercise_14/exercise_14.sql
+++ exercise_14/exercise_14.sql
@@ -1,2 +1,8 @@
 -- the average film length by category
 -- include the category name and avg length
+select c.name, avg(f.length)
+from film f
+join film_category fc on f.film_id = fc.film_id
+join category c on c.category_id = fc.category_id
+group by c.name
+limit 10;
